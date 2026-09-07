d3559b8393e68f61010d41ed5ca62a8c42a09d36
Andy Diaz <asdiazm@uamv.edu.ni>
2026-09-07 16:17:42 -0600

    Update Producto

--- FacturacionApp/src/main/java/ni/uam/edu/facturacionapp/model/Producto.java
+++ FacturacionApp/src/main/java/ni/uam/edu/facturacionapp/model/Producto.java
@@ -1,4 +1,24 @@
 package ni.uam.edu.facturacionapp.model;
 
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+
 public class Producto {
+
+    private Integer id;
+    private String codigo;
+    private String nombre;
+    private Categoria categoria;
+    private BigDecimal precioVenta;
+    private int existencia;
+    private String rutaImagen;
+    private boolean activo;
+
 }
