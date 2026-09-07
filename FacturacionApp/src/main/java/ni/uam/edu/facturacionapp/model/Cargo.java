33ff88eb68357c041d7f4edc932f00cc746654f6
Andy Diaz <asdiazm@uamv.edu.ni>
2026-09-07 16:18:46 -0600

    Update Cargo.java

--- FacturacionApp/src/main/java/ni/uam/edu/facturacionapp/model/Cargo.java
+++ FacturacionApp/src/main/java/ni/uam/edu/facturacionapp/model/Cargo.java
@@ -1,4 +1,15 @@
 package ni.uam.edu.facturacionapp.model;
 
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+
 public class Cargo {
+    private Integer id;
+    private String nombre;
+    private String description;
 }
