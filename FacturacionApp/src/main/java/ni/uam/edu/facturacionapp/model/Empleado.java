f78c39c625c358f0abcdfefa811bfe53b499b962
Andy Diaz <asdiazm@uamv.edu.ni>
2026-09-07 16:22:16 -0600

    Update Empleado.java

--- FacturacionApp/src/main/java/ni/uam/edu/facturacionapp/model/Empleado.java
+++ FacturacionApp/src/main/java/ni/uam/edu/facturacionapp/model/Empleado.java
@@ -1,4 +1,21 @@
 package ni.uam.edu.facturacionapp.model;
 
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Empleado {
+
+    private Integer id;
+    private String nombre;
+    private String apellidos;
+    private Cargo cargo;
+    private LocalDate fechaContratacion;
+    private boolean activo;
 }
